3b30bab3eb1299fe76a2543217729f454ee995dd
BUXTON Daniel J <b018939i@student.staffs.ac.uk>
2019-10-01 16:52:15 +0100

    Add project files.

---
+++ SimpleServer/SimpleServer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleServer
+{
+    class SimpleServer
+    {
+        private TcpListener tcpListener { get; set; }
+
+        public SimpleServer(string ipAddress, int port)
+        {
+            tcpListener = new TcpListener(IPAddress.Parse(ipAddress), port);
+        }
+
+        public void Start()
+        {
+            tcpListener.Start();
+        }
+
+        public void Stop()
+        {
+            tcpListener.Stop();
+        }
+
+        private void SocketMethod(Socket socket)
+        {
+
+        }
+
+        private string GetReturnMessage(string code)
+        {
+            return "";
+        }
+    }
+}
